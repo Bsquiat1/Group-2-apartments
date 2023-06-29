00019a4c3d91bf6e10a65467f0e8e0b119a8a01a
Bryan Waweru <111865501+Bwaweru12345@users.noreply.github.com>
2023-06-29 12:19:25 +0300

    Bryan routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do 
   resources:leases, only: [:create, :destroy] 
   resources:tenants, only: [:index, :show, :create, :update, :destroy] 
+  resources:apartments, only: [:index, :show, :create, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html 
   # Defines the root path route ("/") # root "articles#index" 
 end
