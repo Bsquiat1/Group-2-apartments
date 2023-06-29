efe85e55425927af6d8e0160d34c6d616a3c35e4
Bwaweru12345 <bryanwaweru1076@example.com>
2023-06-29 11:59:32 +0300

    Bryan: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do 
+  resources:leases, only: [:create, :destroy] 
+  resources:tenants, only: [:index, :show, :create, :update, :destroy] 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html 
+  # Defines the root path route ("/") # root "articles#index" 
 end
